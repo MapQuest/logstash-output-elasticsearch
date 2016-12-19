5182c35206e7f9fb85cbbdfe26b55413fca3169f
brian.holland <brian.holland@mapquest.com>
2016-12-19 10:48:11 -0700

    remove trailing comma

--- lib/logstash/outputs/elasticsearch/common.rb
+++ lib/logstash/outputs/elasticsearch/common.rb
@@ -142,7 +142,7 @@ module LogStash; module Outputs; class ElasticSearch;
         :_id => @document_id ? event.sprintf(@document_id) : nil,
         :_index => event.sprintf(@index),
         :_type => type,
-        :_routing => @routing ? event.sprintf(@routing) : nil,
+        :_routing => @routing ? event.sprintf(@routing) : nil
       }
 
       if @pipeline
